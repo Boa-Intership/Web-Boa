037fab11079a92514db4523cec1154acde369cdd
and312 <andreapelaez436@gmail.com>
2025-10-22 20:39:02 -0400

    [UPDATE] envío de datos de preregistro

--- src/features/office/data/services/office.service.ts
+++ src/features/office/data/services/office.service.ts
@@ -1,8 +1,6 @@
 import { strapiClient } from '@/config/httpClient';
 import { Oficina } from '../models/office.model';
 
-//const API_URL = 'http://localhost:1337/api/oficinas?populate=*';
-
 export const OfficeService = {
   async getAll(): Promise<Oficina[]> {
     //const res = await axios.get(API_URL);
